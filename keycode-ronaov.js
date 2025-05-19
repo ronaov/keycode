ca4514cfb6e1fd6c4ee46516310f61b6b8e84bca
ronaov <giangthongthai2006@gmail.com>
2025-05-19 13:44:09 +0700

    update

---
+++ keycode-ronaov.js
@@ -0,0 +1,39 @@
+const btn = document.getElementById("getcode")
+const frame = document.getElementById("frame")
+const p = document.getElementById("key")
+const param = new URLSearchParams(window.location.search)
+const key = parseInt(param.get("key"))
+
+function decimalToString(dec) {
+	let hex = dec.toString(16).padStart(20, "0")
+
+	let result = "";
+	for (let i = 0; i < hex.length; i += 2) {
+		let part = hex.substring(i, i + 2);
+		let charCode = parseInt(part, 16);
+		result += String.fromCharCode(charCode);
+	}
+	return result;
+}
+
+btn.addEventListener("click", function() {
+  frame.style.height = "100px"
+  p.textContent = decimalToString(key)
+  btn.style.width = "80%"
+  btn.style.color = "rgba(255, 255, 255, 0)"
+  
+  setTimeout( () => {
+    p.style.opacity = "1"
+  }, 300)
+  
+  setTimeout( () => {
+  	btn.textContent = "CHẠM VÀO MÃ ĐỂ SAO CHÉP"
+  	btn.style.color = "rgba(255, 255, 255, 1)"
+  }, 100)
+})
+
+p.addEventListener("click", function() {
+	navigator.clipboard.writeText(decimalToString(key))
+		.then(() => alert("Đã sao chép"))
+		.catch(() => alert("Không thể sao chép"))
+})
